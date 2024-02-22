112d7f4d12eb13bac40254246ecfd78a7b998295
s_kusakabe <s_kusakabe@stagegroup.jp>
2024-02-21 23:41:08 -0800

    :pencil2: Fix a typo

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchLatestInvoices } from '@/app/lib/data';
 export default async function LatestInvoices() {
-  const latestInvoices - await fetchLatestInvoices();
+  const latestInvoices = await fetchLatestInvoices();
 
   return (
     <div className="flex w-full flex-col md:col-span-4 lg:col-span-4">
